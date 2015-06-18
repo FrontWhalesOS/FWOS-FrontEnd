3171781f055ee3bcb4480713ea691039a5fa958f
Joe Joyave <jjoyave1@gmail.com>
2015-06-18 00:02:31 -0400

    js: added functionality to main.router.js, main.js, main.view.js added html for top nav bar with styling

--- app/js/views/main.view.js
+++ app/js/views/main.view.js
@@ -13,7 +13,58 @@
 
     },
 
-    template: hbs.main
+    template: hbs.main, // main = template for login/register page
+
+    initialize: function(options) {
+
+      var args = options || {};
+
+      this.collection = args.collection;
+
+      this.render();
+      $('.container').html(this.el);
+    },
+
+    render : function(event) {
+
+      event.preventDefault();
+
+      //registration form values
+      var self = this, form = $(event.target),
+          user_name = form.find('#new-username').val(),
+          full_name = form.find('#new-name').val(),
+          email = form.find('#new-email').val();
+
+          //password verify function
+      var password = function() {
+
+        var intended = form.find('#new-pwd').val(),
+            verify = form.find('#ver-pwd').val();
+
+        if (intended == verify) {
+          return intended;
+        } else {
+          $('.pwError').html('Your passwords do not match.');
+        };
+      }
+
+
+      //create user instance on registration
+      var u = new app.Models.User({
+
+        user_name : user_name,
+        password : password,
+        full_name : full_name,
+        email : email
+
+      });
+
+      //add instance to collection and push to database
+      this.collection.add(u).save().success( function() {
+        self.render();
+      });
+
+    }
 
   });
 
